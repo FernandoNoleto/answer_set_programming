ade0d7945e9f78ca3addddb4ecc3ff222e82b6b1
FernandoNoleto <fernandoleto@hotmail.com>
2017-11-12 13:12:41 -0300

    opção de escolher mapa dos estados brasileiros pronta

--- coloração_mapa_do_Brasil/exec.sh
+++ coloração_mapa_do_Brasil/exec.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-mapa_estados_brasileiros.lp >> graph_coloring.lp
+echo 'Escreva qual mapa você quer colorir'
+echo '1 - Estados brasileiros'
+echo '2 - America do Sul'
 
-clingo graph_coloring.lp >> resultado.txt
+read opc
 
-gcc -o coloracao_mapa_brasil coloracao_mapa_brasil.c
+#testando se o campo digitado foi nulo!
+if [ -z $opc ]; then
+  echo "ERRO!"
+  echo "Parâmetro inválido!!!"
+  sleep 1
 
-./coloracao_mapa_brasil
+elif [ $opc -eq 1 ]; then #opção == 1 mapa dos estados brasileiros
+  cat mapa_estados_brasileiros.lp >> graph_coloring.lp
+  clingo graph_coloring.lp >> resultado.txt
+  gcc -o coloracao_mapa_brasil coloracao_mapa_brasil.c
+  ./coloracao_mapa_brasil
+  rm -f coloracao_mapa_brasil
+  rm -f resultado.txt
+
+elif [ $opc -eq 2 ]; then #opção == 2 mapa da América do Sul
+  cat mapa_america_do_sul.lp >> graph_coloring.lp
+  clingo graph_coloring.lp >> resultado.txt
+  gcc -o coloracao_mapa_brasil coloracao_mapa_brasil.c
+  ./coloracao_mapa_brasil
+  rm -f coloracao_mapa_brasil
+  rm -f resultado.txt
+
+else
+  echo "ERRO!"
+  echo 'Opção inválida...'
+  sleep 1
+fi
