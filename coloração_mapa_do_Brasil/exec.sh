e89142561d4fa465df866b0fdd4121bb45372475
FernandoNoleto <fernandoleto@hotmail.com>
2017-11-12 13:22:46 -0300

    pequenas melhorias noarquivo shell script

--- coloração_mapa_do_Brasil/exec.sh
+++ coloração_mapa_do_Brasil/exec.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo 'Escreva qual mapa você quer colorir'
-echo '1 - Estados brasileiros'
-echo '2 - America do Sul'
+echo ''
+echo 'Qual mapa você quer colorir?'
+echo '1 -> Mapa do Brasil'
+echo '2 -> Mapa da América do Sul'
 
 read opc
 
@@ -13,20 +14,26 @@ if [ -z $opc ]; then
   sleep 1
 
 elif [ $opc -eq 1 ]; then #opção == 1 mapa dos estados brasileiros
+  cat graph_coloring.lp >> aux.txt
   cat mapa_estados_brasileiros.lp >> graph_coloring.lp
   clingo graph_coloring.lp >> resultado.txt
   gcc -o coloracao_mapa_brasil coloracao_mapa_brasil.c
   ./coloracao_mapa_brasil
   rm -f coloracao_mapa_brasil
   rm -f resultado.txt
+  cat aux.txt > graph_coloring.lp
+  rm -f aux.txt
 
 elif [ $opc -eq 2 ]; then #opção == 2 mapa da América do Sul
+  cat graph_coloring.lp >> aux.txt
   cat mapa_america_do_sul.lp >> graph_coloring.lp
   clingo graph_coloring.lp >> resultado.txt
   gcc -o coloracao_mapa_brasil coloracao_mapa_brasil.c
   ./coloracao_mapa_brasil
   rm -f coloracao_mapa_brasil
   rm -f resultado.txt
+  cat aux.txt > graph_coloring.lp
+  rm -f aux.txt
 
 else
   echo "ERRO!"
